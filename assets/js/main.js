b1063580b02eea8d4653b9d10c66bc2cb62e482e
Alex <jonk6233@gmail.com>
2020-11-01 13:02:24 +0200

    reworked contact me

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,5 +100,12 @@
     offset: '80%'
   });
 
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
 
 })(jQuery);
